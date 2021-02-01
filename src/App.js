833425b10e89ebd8bab8dac7daa04f9d8677496d
lostways <andrew@lostways.com>
2021-02-01 11:08:36 +0000

    more ui improvments

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Link,
 } from 'react-router-dom';
 import SearchParams from './SearchParams';
 import PointCards from './PointCards';
 import { 
     Container,
     Header,
+    Menu,
+    Image,
 } from 'semantic-ui-react';
 
 import './App.css';
@@ -17,8 +20,20 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <Header className='App-header' as='h1' content='Asana Story Points' textAlign='center' />
-      <Container className="container">
+      <Menu fixed="top" borderless={true} inverted>
+        <Container>
+          <Menu.Item as="a" href="/" header>
+            <Image
+              size="mini"
+              src="/static/images/logo-100.png"
+              style={{ marginRight: "1.5em" }}
+            />
+            Asana Story Points
+          </Menu.Item>
+        </Container>
+      </Menu>
+      <Container style={{ marginTop: "7em" }} className="container">
+        <Header className='App-header' as='h1' content='Asana Story Points' textAlign='center' />
         <Router>
             <Switch>
                 <Route path="/points/:sectionId">
